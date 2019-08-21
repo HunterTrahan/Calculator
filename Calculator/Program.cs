034a5c632287cd8191f4abfc1e77e3a4a77d3a74
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-08-21 17:55:04 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This will more than likely go no where
+
+namespace Calculator
+{
+    class Program
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I do not know what i'm doing so here is a blackbox with text");
+            Console.ReadLine();
+        }
+    }
+}
